5a444636be1c17534250c127cd4d5678258b7011
Behruz <behruznorov097@gmail.com>
2024-07-13 11:22:39 +0500

    Initial Commit

---
+++ SoccerBetting/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SoccerBetting
+//
+//  Created by Behruz Norov on 13/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
